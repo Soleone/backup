4a394bcb2aeac0601b4fb5fc9a8226bb50710561
Dennis Theisen <dennis@jadedpixel.com>
2009-10-13 00:21:46 +0200

    Started "say" command line rap textfile. Added html5drum kit pattern (for use with website via link)

---
+++ say_rap.rb
@@ -0,0 +1,9 @@
+# command line gangster rap
+# osx only:
+verses = []
+
+verses << "all you mother fuckers can kiss my ass bitch why ask just give me some head and shut the fuck up stupid slut sometimes you see me in jeans then that means bloody streets i'm shooting on nerdy freaks they're so weak it's a treat the first one goes down with a blow he's k o yes yo the next one gets hit straight in his face it is a waste"
+verses << "Betty has a pretty ass jiggy titts shitty hats and its no surprise she wants to get bitch slapped by me she will suck with killer lips on those dicks its her shit she wants to make money and then i guess its okay but the more i hear her voice have no choice call the boys we going to party like what the fucks going on"
+verses << "Sitting in my lower ride all the night overtight feels like on a roller ride cause i am fucking high i dont know whats left to do wrestle you that should do and the fucking rescue crew gets a load of my shot gun"
+
+`say -v Cello "#{verses.join(' ')}"`
